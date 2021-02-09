d513d371d4d5e2720b69cbf01905292fee5c6712
Brian Yip <brianyip@Brians-MacBook-Air.local>
2021-02-09 15:03:57 -0700

    Setup: - Infrastructure to read websocket in ruby - basic skeleton for first tests - TODO: -- refactor ruby websocket client for dynamic setting of websocket url -- add reminder tests, list reminder tests, clear reminder tests

---
+++ server/spec/lib/chat.rb
@@ -0,0 +1,33 @@
+require 'websocket-client-simple'
+require_relative 'websocket-client-simple-patch'
+
+class Chat
+  attr_reader :ws
+
+  def initialize
+    @response_msg = "no message"
+    @ws = WebSocket::Client::Simple.connect 'ws://localhost:4444'
+
+    @ws.on :message do |msg|
+      @response_msg = msg.data
+    end
+
+    @ws.on :open do
+      puts "opening connection"
+      @response_msg = "opened connection"
+    end
+
+    @ws.on :close do |e|
+      p e
+      exit 1
+    end
+
+    @ws.on :error do |e|
+      p e
+    end
+  end
+
+  def send_message(msg)
+    @ws.send msg
+  end
+end
---
+++ server/spec/reminders_add_spec.rb
@@ -0,0 +1,30 @@
+describe "Add Reminders" do
+
+  describe "test response" do
+    it "is stubbed" do
+      pending("something else getting finished")
+      fail
+    end
+  end
+
+  describe "with minutes reminder" do
+    it "is stubbed" do
+      pending("something else getting finished")
+      fail
+    end
+  end
+
+  describe "with hour reminders" do
+    it "is stubbed" do
+      pending("TO BE IMPLEMENTED")
+      fail
+    end
+  end
+
+  describe "with mix of hour and minute reminders" do
+    it "is stubbed" do
+      pending("TO BE IMPLEMENTED")
+      fail
+    end
+  end
+end
